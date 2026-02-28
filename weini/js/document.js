446bdd36a8ce26264d7a3fb1940a2d28645c6907
bbwinnie <nini200702@gmail.com>
2026-02-28 18:15:19 -0500

    add the input bar

--- weini/js/document.js
+++ weini/js/document.js
@@ -55,28 +55,60 @@ function setup() {
         grid.appendChild(cell);
 
 
-        let question = document.createElement("div");
-        question.style.position = "absolute";
-        question.style.left = "120px";
-        question.style.top = "70px";
-        question.style.transform = "translateY(-50%)";
-        question.style.color = "#00FF00";
-        question.style.fontSize = "5px";
-        question.style.fontFamily = "Space Mono";
-        question.style.zIndex = "15";
-        question.style.textAlign = "left";
-        question.style.justifyContent = "left";
-        cell.appendChild(question);
+        const boxesWithQuestion = [0, 1, 2]; // 想出现在哪些框就写哪些 index
 
-        let questionsText = [
-            "What is your name?",
-            "How are you feeling today?",
-            "What are you doing right now?"
-        ];
+        if (boxesWithQuestion.includes(i)) {
+            let questionsText = [
+                "What is your name?",
+                "How are you feeling today?",
+                "What are you doing right now?"
+            ];
 
-        let currentQuestion = 0;
+            let currentQuestion = 0;
 
-        question.innerHTML = questionsText[currentQuestion];
+            let question = document.createElement("div");
+            question.innerHTML = questionsText[currentQuestion];
+            question.style.position = "absolute";
+            question.style.left = "120px";
+            question.style.top = "70px";
+            question.style.transform = "translateY(-50%)";
+            question.style.color = "#00FF00";
+            question.style.fontSize = "5px";
+            question.style.fontFamily = "Space Mono";
+            question.style.zIndex = "15";
+            question.style.textAlign = "left";
+            question.style.justifyContent = "left";
+            cell.appendChild(question);
+
+
+
+            let inputText = document.createElement("div");
+            inputText.classList.add("inputText");
+            cell.appendChild(inputText);
+
+            let inputAnswer = document.createElement("input");
+            inputAnswer.setAttribute("type", "text");
+            inputAnswer.setAttribute("id", `inputField_${i}`);
+            inputAnswer.style.position = "absolute";
+            inputAnswer.style.left = "120px";
+            inputAnswer.style.top = "200px";
+            inputAnswer.style.transform = "translateY(-50%)";
+            inputAnswer.style.width = "200px"
+            inputAnswer.style.border = "none";
+            inputAnswer.style.background = "rgba(212, 212, 212, 0.3)";
+            inputAnswer.style.color = "#00FF00";
+            inputAnswer.style.fontFamily = "Space Mono";
+            inputAnswer.style.fontSize = "5px";
+            inputAnswer.style.caretColor = "#00FF00";
+            inputText.appendChild(inputAnswer);
+
+
+
+
+
+        }
+
+
 
         // input.addEventListener("keydown", function (event) {
         //     if (event.key === "Enter") {
